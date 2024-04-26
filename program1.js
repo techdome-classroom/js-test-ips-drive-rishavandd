1f1dbf763a8285cf17e720c35f20d44f73fbdc60
rishavandd <168168807+rishavandd@users.noreply.github.com>
2024-04-26 13:26:11 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,22 @@
-function smallestMissingPositiveInteger(nums) {
+function firstMissingPositive(nums) {
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        }
+    }
+
+   
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
 
 
-    // Implement the function smallest_missing_positive_integer
-  
-  }
-  
-  module.exports = smallestMissingPositiveInteger;
+
 
